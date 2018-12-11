618b0a6d2de33a44333134f14fcb0f739732eda8
Chris Athanas <realityexpanderdev@gmail.com>
2018-12-11 09:41:04 -0600

    Update rockPaperPermutation.js

--- rockPaperPermutation.js
+++ rockPaperPermutation.js
@@ -2,9 +2,21 @@ function rockPaperPermutation (roundCount) {
   res = [];
   src = ['r', 'p', 's'];
   
-  for(let i=0; i<roundCount; i++) {
-      
+  
+  
+  function roundPick( round, roundNum) {
+    for(let i=0; i<src.length; i++) {
+      round.push(src[i]);
+      if(roundNum == roundCount){
+        
+      } else {
+        
+      }
+      round.pop();
+    }
   }
   
+  roundPick([], 1);
+  return res;
   
 }
